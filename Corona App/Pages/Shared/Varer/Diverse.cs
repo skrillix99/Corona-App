87a62c8806c518affda2f24ad7491b5af2d694a5
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2021-11-30 20:56:49 +0100

    added classes and razer pages

---
+++ Corona App/Pages/Shared/Varer/Diverse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corona_App.Pages.Shared.Varer
+{
+    public class Diverse
+    {
+    }
+}
