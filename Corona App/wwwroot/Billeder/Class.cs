d33451922c76154cdad1c739883b911692364fba
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2021-12-03 13:14:06 +0100

    mappe tilføjet

---
+++ Corona App/wwwroot/Billeder/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corona_App.wwwroot.Billeder
+{
+    public class Class
+    {
+    }
+}
