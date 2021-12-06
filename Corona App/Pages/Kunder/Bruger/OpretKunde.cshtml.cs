c3d545ade3b94792b5a966c9214d6abff9515ac4
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2021-12-06 10:52:06 +0100

    opdateret samling

---
+++ Corona App/Pages/Kunder/Bruger/OpretKunde.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Corona_App.Pages.Kunder.Bruger
+{
+    public class OpretKundeModel : PageModel
+    {
+        [BindProperty]
+        public BrugerInfo b { get; set; }
+
+        private IKunde _kunde;
+
+        public OpretKundeModel(IKunde k)
+        {
+            _kunde = k;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _kunde.Create(b);
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
