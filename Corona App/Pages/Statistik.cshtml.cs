2324db5ac5631f2d57c847701699790fc17eacea
Anders Zune Sander Petersen <ande939h@edu.easj.dk>
2021-12-13 15:50:43 +0100

    Statistik over antal af brugere (Ikke designet eller sat flot op)

---
+++ Corona App/Pages/Statistik.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Corona_App.Pages.Kunder.Bruger;
+using Corona_App.Pages.Kunder;
+
+namespace Corona_App.Pages
+{
+    public class StatistikModel : PageModel
+    {
+        private string _filename = @"wwwroot\Kunde.json";
+       
+
+        public List<BrugerInfo> Hent() //Læser json filen og henter information fra den
+        {
+            return BrugerCRUD.JsonFileRead(_filename);
+        }
+
+
+        public int AntalBrugere
+        {
+            get { return Hent().Count; }
+        }
+        public void OnGet()
+        {
+            
+        }
+    }
+}
